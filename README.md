b726d4df32ecc71985801cf6c618c1632521a8b6
Gabriella Vidal <gabriellavidal2013@gmail.com>
2019-08-29 21:04:05 -0300

    primeiros passos - o que foi desenvolvido ate aqui

---
+++ README.md
@@ -0,0 +1,46 @@
+# API REST com NodeJS
+
+Para começar o projeto digite 
+```bash 
+$ npm init -y 
+```
+
+## Instalação de pacotes 
+
+Gerenciador de rotas 
+```
+$  yarn add express
+``` 
+
+Para manipular json nas requisições 
+```
+$  yarn add body-parser 
+``` 
+
+Conexão com o banco de dados MongoDB
+```
+$ yarn add mongoose
+``` 
+
+Tokens de autenticação da API 
+```
+$ yarn add jsonwebtoken 
+``` 
+
+Comunicação com o _broker_ MQTT
+```
+yarn add async-mqtt
+```
+
+# Ferramentas 
+
+Realizando testes de requisições: 
+* Insomnia, https://support.insomnia.rest/ 
+
+# Referências 
+
+1. API NodeJS + Express + Mongo, Estrutura e cadastro | Diego Fernandes, https://www.youtube.com/watch?v=BN_8bCfVp88&list=PL85ITvJ7FLoiXVwHXeOsOuVppGbBzo2dp 
+1. Pacote MQTT para NodeJS, https://www.npmjs.com/package/mqtt
+1. Pacote com suporte a async e await, https://github.com/mqttjs/async-mqtt
+1. Documentação introdutório para o MQTT em NodeJS, http://www.steves-internet-guide.com/using-node-mqtt-client/
+1. Express.js Fundamentals - 6 - Middleware Explained, https://www.youtube.com/watch?v=9HOem0amlyg
---
+++ src/controllers/deviceController.js
@@ -0,0 +1,184 @@
+const express = require('express'); 
+const MQTT = require("async-mqtt");
+
+const authMiddleware = require('../middlewares/auth');
+const mqttConfig = require('../config/mqtt_server.json');
+
+const Device = require('../models/device'); 
+
+const router = express.Router(); 
+
+router.use(authMiddleware); 
+
+router.get('/', async (req, res) => {
+    console.log("oiii2");
+    try {
+        const devices = await Device.find(); 
+        return res.send({ devices }); 
+    } catch (err) {
+        return res.status(400).send({error: 'Error loading devices'}); 
+    }
+     
+}); 
+
+router.get('/:deviceId', async (req, res) => {
+    console.log("oiii3");
+    try {
+        const device = await Device.findById(req.params.deviceId); 
+        return res.send({device})
+    } catch (err) {
+        return res.status(400).send({error: 'Error loading device'}); 
+    }
+});
+
+router.post('/', async (req, res) => {
+    console.log("oiii");
+    try {
+        const device = await Device.create(req.body); 
+        return res.send({device})
+    } catch (err) {
+        return res.status(400).send({error: 'Error creating new device'}); 
+    }
+});
+
+
+
+router.put('/movimentar', async  (req, res) => {
+    console.log("oii66")
+    let topicPublish; 
+    let result = "ok"; 
+    let value = req.body.value;
+    // console.log(req.body.user);
+    // console.log(req.body.device);
+    try {
+        const device = await Device.findById(req.body.device);
+        topicPublish = device.topicToWrite;  
+        // console.log(device);
+        console.log(topicPublish);
+    } catch (err) {
+        return res.status(400).send({error: 'Error loading device'}); 
+    } 
+
+    console.log("Valor "+topicPublish+" "+ value + " "+mqttConfig.mqtt_user+" "+
+        mqttConfig.mqtt_password); 
+    try {
+        
+
+        const client = MQTT.connect(mqttConfig.mqtt_server,
+            {
+                clientId: req.body.user,
+                username: mqttConfig.mqtt_user,
+                password: mqttConfig.mqtt_password,
+            }
+        );
+
+        // console.log(client);
+        //When passing async functions as event listeners, make sure to have a try catch block
+        const doStuff = async () => {
+            try {
+                console.log("oiipub");
+                await client.publish(topicPublish, value); 
+                // This line doesn't run until the server responds to the publish
+                await client.end();
+                // This line doesn't run until the client has disconnected without error
+            } catch (e){
+                // Do something about it!
+                result = 'Error on connecting and publishing';
+                console.log(e.stack);
+                process.exit();
+                
+            }
+        }
+
+        client.on("connect", doStuff);
+        return res.send({ result: result }); 
+            
+    } catch (err) {
+        console.log(err);
+        return res.status(400).send({error: 'Error on publishing'}); 
+    }
+});
+
+router.post('/:deviceId/', async  (req, res) => {
+    console.log("oiii4");
+    let topicPublish; 
+    let result = "ok"; 
+    let value = req.body.value;
+    try {
+        const device = await Device.findById(req.params.deviceId);
+        topicPublish = device.topicToWrite;  
+    } catch (err) {
+        return res.status(400).send({error: 'Error loading device'}); 
+    } 
+
+    console.log("Valor "+topicPublish+" "+ value + " "+mqttConfig.mqtt_login+" "+mqttConfig.mqtt_password); 
+    try {
+        
+
+        const client = MQTT.connect(mqttConfig.mqtt_server,
+            {
+                clientId: req.body.user,
+                username: mqttConfig.mqtt_login,
+                password: mqttConfig.mqtt_password,
+            }
+        );
+        // When passing async functions as event listeners, make sure to have a try catch block
+        const doStuff = async () => {
+            try {
+                await client.publish(topicPublish, value); 
+                // This line doesn't run until the server responds to the publish
+                await client.end();
+                // This line doesn't run until the client has disconnected without error
+            } catch (e){
+                // Do something about it!
+                result = 'Error on connecting and publishing';
+                console.log(e.stack);
+                process.exit();
+                
+            }
+        }
+
+        client.on("connect", doStuff);
+        return res.send({ result: result }); 
+            
+    } catch (err) {
+        return res.status(400).send({error: 'Error on publishing'}); 
+    }
+}); 
+
+
+
+router.put('/:deviceId', async (req, res) => {
+console.log("oiii3666212");
+    
+    try {
+        Device.findById(req.params.deviceId, (err, device) => {
+            // This assumes all the fields of the object is present in the body.
+            device.name = req.body.name;
+            device.description = req.body.description; 
+            device.topicPublish = req.body.topicPublish; 
+            device.topicToWrite = req.body.topicToWrite; 
+        
+            device.save((saveErr, updatedDevice) => {
+                res.send({ device: updatedDevice });
+            });
+        });
+    } catch (err) {
+        return res.status(400).send({error: 'Error updating device'}); 
+    } 
+});
+
+router.delete('/:deviceId', async (req, res) => {
+    console.log("oiii99999");
+    try {
+        await Device.findByIdAndRemove(req.params.deviceId); 
+        return res.send({ result: "ok" })
+    } catch (err) {
+        return res.status(400).send({error: 'Error deleting device'}); 
+    }
+});
+
+
+
+module.exports = app => app.use('/device', router); 
+
